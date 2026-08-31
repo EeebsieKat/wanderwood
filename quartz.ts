b69290c242288c363263286107155cc306368438
EeebsieKat <eeebsiekatart@gmail.com>
2026-08-31 05:10:32 -0600

    Trying again

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,5 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import BlockModel from "./quartz/components/BlockModel"
 
 const config = await loadQuartzConfig()
 export default config
